bdbd8617ed0b12ccdfaf6565e191382870487baf
Sam Freeman <samcfreeman888@gmail.com>
2022-09-29 13:48:03 -0500

    getting ready to merge with Dan's image item branch

--- src/components/FavPage/FavPage.js
+++ src/components/FavPage/FavPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {useState} from 'react';
 
 function FavPage() {
    const dispatch = useDispatch();
     const favList = useSelector(store => store.favListReducer);
+    const categoryList = useSelector(store => store.categoriesReducer);
     //will use above to get favs after I GET/set them
+    const [selected, setSelected] = useState('');
 
     useEffect(() => {
         fetchFavList();
@@ -28,6 +31,17 @@ function FavPage() {
 
     }
 
+    //for PUT route to change category
+    //Called on Button click"
+    const setImageCategory = () => {
+        dispatch({
+            type: 'SAGA.SET_CATEGORY'
+        })
+    }
+
+    const submit = () =>{
+        console.log(selected);
+    }
 
     
     return (
@@ -35,11 +49,31 @@ function FavPage() {
         <h1>I'm the FavPage</h1>
         {favList.map(fav =>{
             return(
-            <img key={fav.id} src={fav.url}></img>
+           <div>
+                <img key={fav.id} src={fav.url}></img>
+            <form>
+      <select
+            value={selected}
+            onChange={e => setSelected(e.target.value)}>
+        {categoryList.map((category) => (
+          <option value={category.id} key={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <button type="button" onClick={submit}> Submit</button>
+    </form>
+    </div>
             )
         })}
         </div>
     );
 }
+
+// {categoryList.map(category =>{
+//     return(
+//         <p key={category.id}>{category.name}</p>
+//     )
+// })}
 
 export default FavPage;
